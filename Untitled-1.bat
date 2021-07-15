b3f60eb1db0aeb8e300b0c068b76579869bcc6e8
wellington0813 <86804640+wellington0813@users.noreply.github.com>
2021-07-15 16:28:44 -0300

    Add files via upload

---
+++ Untitled-1.bat
@@ -0,0 +1,204 @@
+ <!DOCTYPE html>
+  <html lang="pt-BR">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Wellington Rodrigues</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+
+<script src="https://kit.fontawesome.com/f0e17cbf2f.js" crossorigin="anonymous"></script>
+<style>
+  .foto-perfil
+  {
+            height: 100%;
+            max-width: auto;
+            border-radius: 50%;
+  }
+  .cor_primaria
+  {
+      background-color:#19bc9c;
+  }
+  .cor_secundaria
+  {
+      background-color:#2c3e50;
+  }
+    p{
+      font-family:lato,'Helvatica Neue',Helvatica,Arial,sans-serif;
+        color:black !important;
+    }
+    .icones
+    {
+      padding-left:5px;
+    }
+    .titulosBrancos
+    {
+      text-decoration:underline;
+      text-underline-position:balow;
+      padding-bottom:20px;
+      text-transform:uppercase;
+      text-weight:700;
+      color:white;
+    }
+    .titulosVerdes
+    {
+      text-decoration:underline;
+      text-underline-position:balow;
+      padding-bottom:20px;
+      text-transform:uppercase;
+      text-weight:700;
+      color:#19bc9c;
+    }
+
+    .texto_branco
+    {
+      color:white !important;
+    }
+
+    .jumbotron
+    {
+      margin-bottom: 0px;
+    }
+
+    .texto_footer
+    {
+      margin:0px;
+      padding-top: 15px;
+      padding-bottom: 15px;
+
+    }
+    html
+    {
+      scroll-behavior: smooth;
+    }
+
+    p
+    {
+     color: black !important;
+    }
+
+</style>
+
+  </head>
+  <body>
+<nav class="cor_secundaria navbar navbar-expand-lg navbar-light">
+  <a class="navbar-brand texto_branco" href="#">Wellington C. Rodrigues</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+ 
+    </ul>
+
+    <ul class="navbar-nav">
+     
+      <li class="nav-item">
+        <a class="nav-link texto_branco" href="#projetos">Projetos</a>
+      </li>
+
+      <li class="nav-item">
+        <a class="nav-link texto_branco" href="#sobre-mim">Sobre mim </a>
+      </li>
+
+      <li class="nav-item">
+        <a class="nav-link texto_branco" href="#depoimentos">Depoimentos</a>
+      </li>
+
+      <li class="nav-item">
+        <a class="nav-link texto_branco" href="#contato">Contato</a>
+      </li>
+    </ul>
+
+  </div>
+</nav>
+    <section class="jumbotron text-center cor_primaria">
+        <div class="container">
+          <img class="foto-perfil" alt="" width="260" height="260" class="avatar avatar-user width-full border color-bg-primary" src="https://avatars.githubusercontent.com/u/86804640?v=4">
+          <h1 class="jumbotron-heading texto_branco">Wellington Rodrigues</h1>
+          <p class="lead text-muted texto_branco">Portfólio inicial</p>
+          <p>
+            <a href="#" class="btn btn-dark my-2 texto_branco">Contato</a>
+          </p>
+          <a href="https://www.linkedin.com/in/wellington-rodrigues-931902184/"><i class="icones fab fa-linkedin"></i></a>
+          <a href="https://github.com/wellington0813"><i class="icones fab fa-github"></i></a>
+         
+    </div>
+</section>
+    <div class="album py-5 bg-light" id="projetos">
+        <div class="container">
+        <h2 class="jumbotron-heading text-center titulosVerdes "> Projetos </h2>
+          <div class="row">
+            <div class="col-md-6">
+              <div class="card mb-4 box-shadow">
+                <img class="card-img-top" data-src="holder.js/100px225?theme=thumb&amp;bg=55595c&amp;fg=eceeef&amp;text=Thumbnail" alt="Thumbnail [100%x225]" style="height: 225px; width: 100%; display: block;" src="  " data-holder-rendered="true">
+                <div class="card-body">
+                  <p class="card-text">...</p>
+                   <ul>
+                                <li>HTML</li>
+                                <li>CSS</li>
+                                <li>PHP</li>
+                            </ul>
+                  <div class="d-flex justify-content-between align-items-center">
+                    <div class="btn-group">
+                      <button type="button" class="btn btn-sm btn-outline-secondary">Ver</button>
+                    </div>
+                    <small class="text-muted">9 mins</small>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+    <div class="col-md-6">
+              <div class="card mb-4 box-shadow">
+                <img class="card-img-top" data-src="holder.js/100px225?theme=thumb&amp;bg=55595c&amp;fg=eceeef&amp;text=Thumbnail" alt="Thumbnail [100%x225]" style="height: 225px; width: 100%; display: block;" src="  " data-holder-rendered="true">
+                <div class="card-body">
+                  <p class="card-text">...</p>
+                  <ul>
+                                <li>HTML</li>
+                                <li>CSS</li>
+                                <li>PHP</li>
+                  </ul>
+                  <div class="d-flex justify-content-between align-items-center">
+                    <div class="btn-group">
+                      <button type="button" class="btn btn-sm btn-outline-secondary">Ver</button>
+                    </div>
+                    <small class="text-muted">9 mins</small>
+                  </div>
+                </div>
+              </div>
+          </div>
+        </div>
+    </div>
+ </div>
+
+    <section class="jumbotron text-center cor_primaria" id="sobre-mim">
+        <div class="container">
+            <h2 class="jumbotron-heading titulosBrancos">Sobre mim</h2>
+            <p class="lead paragrafoBrancos"> Meu nome é Wellington, tenho 20 anos estou atualmente cusando o 6° termo de Analise e Desenvolvimento de Sistemas na FATEC ourinhos, no momento estou fazendo estagio na parte de tecnologia da prefeitura de ourinhos onde atuo no suporte tecnico
+             
+              </p>
+        </div>
+    </section>
+    <section  id="contato">
+    <div>
+      <div class="container-fluid">
+        <h3 style="padding-top: 60px" class="text-center titulosVerdes">Disponivel para oportunidades</h3>
+        <a href="mailto:2607wellington@gmail.com"><h4 style="padding-bottom: 60px; margin-bottom: 0px;" class="text-center">👉 2607wellington@gmail.com 👈</h4></a>
+      </div>
+    </div>
+  </section>
+  <footer class="text-center cor_secundaria texto_footer" style="background-color:#2c3e50;padding:0px; margin:0px">
+        <p style="margin:0px;padding-top:15px;padding-bottom:15px;color:white !important">Copyright - Wellington0813®
+        </p>
+    </footer>
+    </body>
+</html>
